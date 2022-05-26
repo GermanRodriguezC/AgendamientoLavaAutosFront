c66dba69828728f41ae1134ab100f9e9218b3321
German Rodriguez <germanhjunior@gmail.com>
2022-05-25 23:39:04 -0500

    Se adicionan pruebas e2e a PUT y DELETE

--- e2e/src/page/cita/cita.po.ts
+++ e2e/src/page/cita/cita.po.ts
@@ -1,16 +1,23 @@
 import { by, element } from 'protractor';
 
 export class CitaPage {
-    private listarCita = element(by.css('app-listar-cita #button'));
-    private inputIdCita = element(by.css('app-crear-cita #id'));
+    private inputHoraCita = element(by.css('app-crear-cita #hora'));
     private inputPlacaCita = element(by.css('app-crear-cita #placa'));
     private inputFechaCita = element(by.css('app-crear-cita #fecha'));
-    private inputFechaListarCita = element(by.css('app-listar-cita #fecha'));
-    private inputHoraCita = element(by.css('app-crear-cita #hora'));
-    private listaCitas = element.all(by.css('app-listar-cita #listaCitas'));
     private guardarCita = element(by.css('app-crear-cita #button'));
+    private inputIdCita = element(by.css('app-crear-cita #id'));
+    private listarCita = element(by.css('app-listar-cita #button'));
+    private inputFechaListarCita = element(by.css('app-listar-cita #fecha'));
+    private listaCitas = element.all(by.css('app-listar-cita #items'));
+    private inputIdCitaBorrar = element(by.css('app-borrar-cita #id'));
+    private borrarCita = element(by.css('app-borrar-cita #button'));
+    private inputActualizarHoraCita = element(by.css('app-actualizar-cita #hora'));
+    private inputActualizarPlacaCita = element(by.css('app-actualizar-cita #placa'));
+    private inputActualizarFechaCita = element(by.css('app-actualizar-cita #fecha'));
+    private actualizarCita = element(by.css('app-actualizar-cita #button'));
+    private inputActualizarIdCita = element(by.css('app-actualizar-cita #id'));
 
-    getMensajeCrear(selector: string) {
+    getMensaje(selector: string) {
         return element(by.css(selector)).getText() as Promise<string>;
     }
 
@@ -26,12 +33,26 @@ export class CitaPage {
         await this.inputIdCita.sendKeys(idCita);
     }
 
+    async ingresarActualizarId(idCita: string) {
+        await this.inputActualizarIdCita.sendKeys(idCita);
+    }
+
+    async ingresarIdBorrar(idCita: string) {
+        await this.inputIdCitaBorrar.sendKeys(idCita);
+    }
+
     async ingresarPlaca(placaCita: string) {
         await this.inputPlacaCita.sendKeys(placaCita);
+    }
+    async ingresarActualizarPlaca(placaCita: string) {
+        await this.inputActualizarPlacaCita.sendKeys(placaCita);
     }
 
     async ingresarFecha(fechaCita: string) {
         await this.inputFechaCita.sendKeys(fechaCita);
+    }
+    async ingresarActualizarFecha(fechaCita: string) {
+        await this.inputActualizarFechaCita.sendKeys(fechaCita);
     }
 
     async ingresarFechaListar(fechaCita: string) {
@@ -41,6 +62,9 @@ export class CitaPage {
     async ingresarHora(horaCita: string) {
         await this.inputHoraCita.sendKeys(horaCita);
     }
+    async ingresarActualizarHora(horaCita: string) {
+        await this.inputActualizarHoraCita.sendKeys(horaCita);
+    }
 
     async contarCitas() {
         return await this.listaCitas.count();
@@ -48,5 +72,12 @@ export class CitaPage {
 
     async clickBotonGuardarCita() {
         await this.guardarCita.click();
+    }
+    async clickBotonActualizarCita() {
+        await this.actualizarCita.click();
+    }
+
+    async clickBotonBorrarCita() {
+        await this.borrarCita.click();
     }
 }
--- e2e/src/test/cita.e2e-spec.ts
+++ e2e/src/test/cita.e2e-spec.ts
@@ -4,7 +4,6 @@ import { CitaPage } from '../page/cita/cita.po';
 describe('workspace-project Cita', () => {
     let page: AppPage;
     let cita: CitaPage;
-
     beforeEach(() => {
         page = new AppPage();
         cita = new CitaPage();
@@ -15,22 +14,53 @@ describe('workspace-project Cita', () => {
         const HORA = '12:00';
         const PLACA = 'ABC123';
         const FECHA = '02/02/2022';
-
         await cita.ingresarPlaca(PLACA);
         await cita.ingresarFecha(FECHA);
         await cita.ingresarHora(HORA);
         await cita.clickBotonGuardarCita();
-        expect(cita.getMensajeCrear('app-crear-cita h1')).toContain('La cita se creó satisfactoriamente con el Id:');
+        expect(cita.getMensaje('app-crear-cita h1')).toContain('La cita se creó satisfactoriamente con el Id:');
     });
 
     it('Deberia listar citas', async () => {
         page.navigateToCita('/listar');
-        const FECHA = '04/02/2022';
+        const FECHA = '02022022';
+        await cita.ingresarFechaListar(FECHA);
+        await cita.clickBotonListarCitas();
+        expect(await cita.contarCitas() > 0).toBe(true);
+    });
 
+    it('No deberia encontrar citas agendadas para la fecha asignada', async () => {
+        page.navigateToCita('/listar');
+        const FECHA = '04022022';
         await cita.ingresarFechaListar(FECHA);
         await cita.clickBotonListarCitas();
         expect(0).toBe(await cita.contarCitas());
-        expect(cita.getMensajeCrear('app-listar-cita h1')).toEqual('No se encontró ninguna cita para esa fecha.');
+        expect(cita.getMensaje('app-listar-cita h1')).toEqual('No se encontró ninguna cita para esa fecha.');
+    });
+
+    it('Deberia borrar la cita', async () => {
+        page.navigateToCita('/borrar');
+        const id = '1';
+        await cita.ingresarIdBorrar(id);
+        await cita.clickBotonBorrarCita();
+        expect(cita.getMensaje('app-borrar-cita h1')).toContain('Se eliminó satisfactoriamente la cita con el id:');
+    });
+
+    it('Debería actualizar cita ', async () => {
+        page.navigateToCita('/actualizar');
+        const ID = '2';
+        const HORA = '12:00';
+        const PLACA = 'ABC123';
+        const FECHA = '02/02/2022';
+        await cita.ingresarActualizarId(ID);
+        await cita.ingresarActualizarPlaca(PLACA);
+        await cita.ingresarActualizarFecha(FECHA);
+        await cita.ingresarActualizarHora(HORA);
+        await cita.clickBotonActualizarCita();
+        expect(ID).toContain(await cita.getMensaje('app-actualizar-cita #idActualizado'));
+        expect(PLACA).toContain(await cita.getMensaje('app-actualizar-cita #placaActualizada'));
+        expect(FECHA).toContain(await cita.getMensaje('app-actualizar-cita #fechaActualizada'));
+        expect(HORA).toContain(await cita.getMensaje('app-actualizar-cita #horaActualizada'));
     });
 
 });
